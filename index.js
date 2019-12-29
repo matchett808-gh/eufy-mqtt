9d89ee751050dbf43f5611cf849c90747c5d128b
Iain Matchett <iain.matchett@beeksfx.com>
2019-12-29 23:45:57 +0000

    added some statics

--- index.js
+++ index.js
@@ -3,86 +3,87 @@ var mqtt = require("mqtt");
 let lib = require('./dist/index');
 const RoboVac = lib.RoboVac;
 
-let robovac_settings = {
-localKey: config.local_key,
-deviceId: config.device_id
-}
-var client = mqtt.connect(config.mqtturl, config.mqttoptions)
+let robovacSettings = {
+    deviceId: config.device_id,
+    localKey: config.local_key
+};
 
-client.on('connect', function () {
-    topics = [];
-    topics.push(config.base_topic+'vacuum/command')
-    topics.push(config.base_topic+'vacuum/set_fan_speed')
-    console.log(topics)
+const r = new RoboVac(robovacSettings);
+var client = mqtt.connect(config.mqtturl, config.mqttoptions);
+
+client.on('connect', () => {
+    let topics = [];
+    topics.push(config.base_topic+'vacuum/command');
+    topics.push(config.base_topic+'vacuum/set_fan_speed');
+    console.log(topics);
     client.subscribe(topics);
-})
+});
 
-client.on('message', function (topic, message) {
+client.on('message', (topic, message) => {
     switch(message.toString()) {
-      case('locate'):
-            r.setFindRobot();
-          break;
-      case('pause'):
-            r.pause();
-          break;
-      case('start'):
-            r.startCleaning();
-          break;
-      case('return_to_base'):
-            r.goHome();
-          break;
-      case('clean_spot'):
-            r.setWorkMode('Spot')
-          break;
+    case('locate'):
+        r.setFindRobot();
+        break;
+    case('pause'):
+        r.pause();
+        break;
+    case('start'):
+        r.startCleaning();
+        break;
+    case('return_to_base'):
+        r.goHome();
+        break;
+    case('clean_spot'):
+        r.setWorkMode('Spot');
+        break;
+    default:
+        throw new Error('Unrecognized command');
     }
 });
 
-const r = new RoboVac(robovac_settings)
-let overall_statep = {}
-let getStatus = async function () {
-    //let r = new RoboVac(robovac_settings);
-    await r.getStatuses()
-    let dps = r.statuses['dps']
-    let statep = {}
-    if(dps['104'] != 'undefined' )
-        statep.battery_level = r.statuses['dps']['104']
-    if(dps['1'] != 'undefined' )
-        statep.docked = r.statuses['dps']['1']
-    if(dps['5'] != 'undefined' )
-        statep.fan_speed = r.statuses['dps']['5']
-    if(dps['106'] != 'undefined' )
-        statep.error = r.statuses['dps']['106']
-    if(dps['15'] != 'undefined' )
-        statep.cleaning = r.statuses['dps']['15']
+let retainedValues = {};
+
+// eslint-disable-next-line
+async function getStatus () {
+                await r.getStatuses();
+                let dps = r.statuses.dps;
+                let statep = {};
+                if(dps['104'] !== 'undefined' ) { 
+                  // eslint-disable-next-line
+                  statep.battery_level = r.statuses.dps['104']; 
+                }
+                if(dps['1'] !== 'undefined' ) { statep.docked = r.statuses.dps['1']; }
+                if(dps['5'] !== 'undefined' ) { 
+                  // eslint-disable-next-line
+                  statep.fan_speed = r.statuses.dps['5']; 
+                }
+                if(dps['106'] !== 'undefined' ) { statep.error = r.statuses.dps['106']; }
+                if(dps['15'] !== 'undefined' ) { statep.cleaning = r.statuses.dps['15']; }
 
 
-    statep.state = 'idle'
-    console.log(dps)
-    if(dps['15'] == 'Recharge') {
-      statep.state = 'returning'
-    } else if(dps['103']) {
-      statep.state = 'idle' // locating
-    }  else if(dps['2']) {
-      statep.state = 'paused'
-    } else if(dps['15'] == 'Running') {
-      statep.state = 'cleaning' 
-    } else if(dps['15'] == 'Charging') {
-      statep.state = 'docked'
-    } else if(dps['106'] != 'no_error' ) {
-      statep.state = 'error'
-    }
-    Object.keys(statep).forEach(key => {
-      if (statep[key] === undefined) {
-         delete statep[key];
-      }
-    });
-    console.log(overall_statep)
-    let a = Object.assign(overall_statep, statep)
-    overall_statep = a
-    client.publish(config.base_topic+'vacuum/state', JSON.stringify(overall_statep), {retain: true})
+                statep.state = 'idle';
+                console.log(dps);
+                if(dps['15'] === 'Recharge') {
+                    statep.state = 'returning';
+                } else if(dps['103']) {
+                    statep.state = 'idle'; 
+                }  else if(dps['2']) {
+                    statep.state = 'paused';
+                } else if(dps['15'] === 'Running') {
+                    statep.state = 'cleaning'; 
+                } else if(dps['15'] === 'Charging') {
+                    statep.state = 'docked';
+                } else if(dps['106'] !== 'no_error' ) {
+                    statep.state = 'error';
+                }
+                Object.keys(statep).forEach((key) => {
+                    if (statep[key] === undefined) {
+                        delete statep[key];
+                    }
+                });
+                let a = Object.assign(retainedValues, statep);
+                retainedValues = a;
+                client.publish(config.base_topic+'vacuum/state', JSON.stringify(retainedValues), {retain: true});
 
-};
-setInterval(getStatus,3000)
-function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
 }
+setInterval(getStatus,3000);
